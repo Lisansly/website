5ab7514ea38e4316b492019f68a37022a5742f44
yali <tangozyusufali@gmail.com>
2023-05-28 14:18:53 +0300

    Updated logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,14 +1,38 @@
-import { Group, Image, Text } from "@mantine/core";
+import { Group, Image, Text, createStyles } from "@mantine/core";
 import { Link } from "react-router-dom";
 
 const Logo = () => {
+  const useStyles = createStyles((theme) => ({
+    link: {
+      textDecoration: "none",
+      color: theme.colorScheme === "dark" ? theme.white : theme.black,
+      marginLeft: theme.spacing.md,
+      fontSize: theme.fontSizes.lg,
+      fontWeight: 700,
+      transition: "color 200ms ease",
+      marginRight: theme.spacing.xl,
+      "&:hover": {
+        color:
+          theme.colorScheme === "dark" ? theme.colors.blue[4] : theme.black,
+      },
+    },
+    group: {
+      display: "flex",
+      flexWrap: "nowrap",
+    },
+    logo: {
+      maxWidth: 40,
+      [theme.fn.smallerThan("xs")]: {
+        display: "none",
+      },
+    },
+  }));
+  const { classes } = useStyles();
   return (
-    <Link to={"/"}>
-      <Group>
-        <Image src={"/lisansly.png"} maw={40} />
-        <Text fw={800} size={"xl"} c={"#1C7ED6"}>
-          Lisansly
-        </Text>
+    <Link to={"/"} className={classes.link}>
+      <Group className={classes.group}>
+        <Image src={"/lisansly.png"} className={classes.logo} />
+        <Text>Lisansly</Text>
       </Group>
     </Link>
   );
