c1c5efe22be0f838bfea2ab1584619f9ecbe6a6d
yali <tangozyusufali@gmail.com>
2023-06-10 00:46:52 +0300

    Scroll to top when pathname changed

--- src/components/Shell.tsx
+++ src/components/Shell.tsx
@@ -2,17 +2,26 @@ import { useIsAuthenticated, useSignOut } from "react-auth-kit";
 import { AppShell, ScrollArea } from "@mantine/core";
 import Navbar from "./navbar/Navbar";
 import Header from "./header/Header";
-
+import React, { useRef, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import Footer from "./Footer";
 type ShellProps = {
   children: React.ReactNode;
 };
 
 const Shell: React.FC<ShellProps> = ({ children }) => {
   const isAuthenticated = useIsAuthenticated();
+  const viewport = useRef<HTMLDivElement>(null);
+  const { pathname } = useLocation();
   const signOut = useSignOut();
+
+  useEffect(() => {
+    viewport.current?.scrollTo(0, 0);
+  }, [pathname]);
 
   return (
     <ScrollArea
+      viewportRef={viewport}
       styles={{
         scrollbar: {
           zIndex: 100,
@@ -23,9 +32,10 @@ const Shell: React.FC<ShellProps> = ({ children }) => {
         sx={(theme) => ({
           main: {
             height: "100vh",
+            padding: 0,
             paddingTop: 140,
             [theme.fn.smallerThan("sm")]: {
-              paddingTop: 110,
+              paddingTop: 95,
             },
           },
         })}
@@ -34,6 +44,7 @@ const Shell: React.FC<ShellProps> = ({ children }) => {
         header={<Header />}
       >
         {children}
+        <Footer />
       </AppShell>
     </ScrollArea>
   );
