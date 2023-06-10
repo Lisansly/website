f07407e9bc77d8e305c3de04b8e60cf721c8f2ba
yali <tangozyusufali@gmail.com>
2023-06-11 00:23:40 +0300

    Added form validation edit profile and change password

--- src/components/profile/ChangePassword.tsx
+++ src/components/profile/ChangePassword.tsx
@@ -3,6 +3,7 @@ import { Group, Paper, Tabs } from "@mantine/core";
 import { useState } from "react";
 import Helper from "../Helper";
 import Button from "../Button";
+import { hasLength, useForm } from "@mantine/form";
 
 const passwordInputs: PasswordInputProps[] = [
   {
@@ -17,19 +18,58 @@ const passwordInputs: PasswordInputProps[] = [
   },
 ];
 
+type ChangePasswordProps = {
+  password: string;
+  confirmPassword: string;
+};
+
 const ChangePassword = () => {
   const [loading] = useState(false);
+
+  const form = useForm<ChangePasswordProps>({
+    initialValues: {
+      password: "",
+      confirmPassword: "",
+    },
+    validate: {
+      password: hasLength(
+        { min: 10 },
+        "Password must be at least 10 characters"
+      ),
+      confirmPassword: (value, values) =>
+        value.length === 0
+          ? "Confirm password is required"
+          : value !== values.password
+          ? "Passwords must match"
+          : null,
+    },
+    validateInputOnChange: true,
+  });
+
+  const onSubmit = (values: ChangePasswordProps) => {
+    console.log(values);
+  };
 
   return (
     <Tabs.Panel value="change-password" pt="xs">
       <Group position="center" mt="xl">
         <Helper />
-        <Paper p="md" m="xs" withBorder radius="md" maw="400px" w="100%">
+        <Paper
+          onSubmit={form.onSubmit((values) => onSubmit(values))}
+          component="form"
+          p="md"
+          m="xs"
+          withBorder
+          radius="md"
+          maw="400px"
+          w="100%"
+        >
           {passwordInputs.map((input) => (
             <PasswordInput
+              validation={form.getInputProps(input.key as string)}
               placeholder={input.placeholder as string}
               label={input.label as string}
-              key={input.key}
+              key={input.key as string}
             />
           ))}
           <Button loading={loading} label="Save" />
--- src/components/profile/EditProfile.tsx
+++ src/components/profile/EditProfile.tsx
@@ -1,8 +1,10 @@
-import { Group, Paper, Tabs } from "@mantine/core";
-import Helper from "../Helper";
+import { isEmail, isNotEmpty, useForm } from "@mantine/form";
 import TextInput, { TextInputProps } from "../TextInput";
-import Button from "../Button";
+import { Group, Paper, Tabs } from "@mantine/core";
+import { useAuthUser } from "react-auth-kit";
 import { useState } from "react";
+import Helper from "../Helper";
+import Button from "../Button";
 
 const textInputs: TextInputProps[] = [
   {
@@ -17,15 +19,48 @@ const textInputs: TextInputProps[] = [
   },
 ];
 
+type EditProfileProps = {
+  name: string;
+  email: string;
+};
+
 const EditProfile = () => {
+  const userData = useAuthUser();
+
+  const form = useForm<EditProfileProps>({
+    initialValues: {
+      name: userData()?.name,
+      email: userData()?.email,
+    },
+    validate: {
+      name: isNotEmpty("Name is required"),
+      email: isEmail("Email must be valid"),
+    },
+    validateInputOnChange: true,
+  });
+
+  const onSubmit = (values: EditProfileProps) => {
+    console.log(values);
+  };
+
   const [loading] = useState(false);
   return (
     <Tabs.Panel value="edit" pt="xs">
       <Group position="center" mt="xl">
         <Helper />
-        <Paper p="md" m="xs" withBorder radius="md" maw="400px" w="100%">
+        <Paper
+          onSubmit={form.onSubmit((values) => onSubmit(values))}
+          component="form"
+          p="md"
+          m="xs"
+          withBorder
+          radius="md"
+          maw="400px"
+          w="100%"
+        >
           {textInputs.map((input) => (
             <TextInput
+              validation={form.getInputProps(input.key as string)}
               placeholder={input.placeholder as string}
               label={input.label as string}
               key={input.key}
