190c24ab94dfdbf1df2003a6fbe34edeba07369a
yali <tangozyusufali@gmail.com>
2023-06-15 00:06:43 +0300

    Scroll to top when pathname changed

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,4 +1,8 @@
-import { Route, Routes as ReactRouterDomRouters } from "react-router-dom";
+import {
+  Route,
+  Routes as ReactRouterDomRouters,
+  useLocation,
+} from "react-router-dom";
 import { RequireAuth, useIsAuthenticated } from "react-auth-kit";
 import Dashboard from "./components/dashboard/Dashboard";
 import PageNotFound from "./components/PageNotFound";
@@ -6,9 +10,15 @@ import Profile from "./components/profile/Profile";
 import SignUp from "./components/auth/SignUp";
 import SignIn from "./components/auth/SignIn";
 import Home from "./components/home/Home";
+import { useEffect } from "react";
 
 const Routes = () => {
   const isAuthenticated = useIsAuthenticated();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return (
     <ReactRouterDomRouters>
