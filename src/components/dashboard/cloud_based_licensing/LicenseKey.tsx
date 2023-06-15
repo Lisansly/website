7d1231c6252ad1ac71e4643773db4135182fc214
yali <tangozyusufali@gmail.com>
2023-06-15 22:41:52 +0300

    Added copy license

--- src/components/dashboard/cloud_based_licensing/LicenseKey.tsx
+++ src/components/dashboard/cloud_based_licensing/LicenseKey.tsx
@@ -1,5 +1,13 @@
-import { ActionIcon, Group, Paper, Text } from "@mantine/core";
-import { IconTool } from "@tabler/icons-react";
+import {
+  ActionIcon,
+  Box,
+  CopyButton,
+  Group,
+  Paper,
+  Text,
+  Tooltip,
+} from "@mantine/core";
+import { IconCheck, IconCopy, IconTool } from "@tabler/icons-react";
 
 type LicenseKeyProps = {
   value: string;
@@ -8,13 +16,31 @@ type LicenseKeyProps = {
 const LicenseKey = (props: LicenseKeyProps) => {
   return (
     <Paper withBorder px={"xs"} py={"5px"} display={"flex"}>
-      <Group spacing={"xs"} w={"100%"}>
+      <Group
+        spacing={"xs"}
+        style={{ display: "flex", width: "100%", flexWrap: "nowrap" }}
+      >
         <Text size={"sm"} style={{ wordBreak: "break-all" }}>
           {props.value}
         </Text>
-        <ActionIcon ml={"auto"}>
-          <IconTool size={"1rem"} />
-        </ActionIcon>
+        <Box ml={"auto"} display={"flex"}>
+          <CopyButton value={props.value} timeout={2000}>
+            {({ copied, copy }) => (
+              <Tooltip label={copied ? "Copied" : "Copy"} withArrow>
+                <ActionIcon color={copied ? "blue" : "gray"} onClick={copy}>
+                  {copied ? (
+                    <IconCheck size="1rem" />
+                  ) : (
+                    <IconCopy size="1rem" />
+                  )}
+                </ActionIcon>
+              </Tooltip>
+            )}
+          </CopyButton>
+          <ActionIcon>
+            <IconTool size={"1rem"} />
+          </ActionIcon>
+        </Box>
       </Group>
     </Paper>
   );
