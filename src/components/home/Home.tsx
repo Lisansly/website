57c15257c6ee51c8e93d3650168fc43ea29f8fb5
yali <tangozyusufali@gmail.com>
2023-06-16 03:23:59 +0300

    Added paper to home

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,23 +1,56 @@
 import { DashboardCard } from "./DashboardCard";
 import { useNavigate } from "react-router-dom";
+import { Group, Paper, createStyles } from "@mantine/core";
 import { HeroHeader } from "./HeroHeader";
 import UsageExample from "./UsageExample";
-import { Group } from "@mantine/core";
 import Helper from "./Helper";
+
+const useStyles = createStyles((theme) => ({
+  paper: {
+    borderRadius: 0,
+    paddingBlock: "50px",
+    height: "max-content",
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.colors.blue[9]
+        : theme.colors.blue[2],
+
+    [theme.fn.largerThan("md")]: {
+      margin: "-10px",
+      transform: "rotate(1deg)",
+      paddingBlock: "100px",
+    },
+  },
+  group: {
+    [theme.fn.largerThan("md")]: {
+      transform: "rotate(-1deg)",
+    },
+  },
+}));
 
 const Home = () => {
   const navigate = useNavigate();
+  const { classes } = useStyles();
 
   return (
-    <div style={{ minHeight: "100vh", gap: "200px", display: "grid" }}>
+    <div
+      style={{
+        minHeight: "100vh",
+        gap: "200px",
+        display: "grid",
+        marginBottom: "300px",
+      }}
+    >
       <Group position="center" spacing={"xl"}>
         <HeroHeader navigate={navigate} />
         <Helper />
       </Group>
-      <Group position="center" spacing={"100px"} mb={"300px"}>
-        <DashboardCard />
-        <UsageExample />
-      </Group>
+      <Paper className={classes.paper}>
+        <Group position="center" spacing={"100px"} className={classes.group}>
+          <DashboardCard />
+          <UsageExample />
+        </Group>
+      </Paper>
     </div>
   );
 };
