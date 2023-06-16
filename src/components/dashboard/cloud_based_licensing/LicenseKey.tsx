6ad6cbfdbc5919ccb602fe2d058edb87975306fd
yali <tangozyusufali@gmail.com>
2023-06-16 23:51:25 +0300

    Updated license key design

--- src/components/dashboard/cloud_based_licensing/LicenseKey.tsx
+++ src/components/dashboard/cloud_based_licensing/LicenseKey.tsx
@@ -6,6 +6,7 @@ import {
   Paper,
   Text,
   Tooltip,
+  createStyles,
 } from "@mantine/core";
 import { IconCheck, IconCopy, IconTool } from "@tabler/icons-react";
 
@@ -13,14 +14,30 @@ type LicenseKeyProps = {
   value: string;
 };
 
+const useStyles = createStyles((theme) => ({
+  paper: {
+    paddingBlock: 5,
+    paddingInline: theme.spacing.sm,
+    display: "flex",
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[9]
+        : theme.colors.gray[0],
+  },
+  text: {
+    wordBreak: "break-all",
+  },
+}));
+
 const LicenseKey = (props: LicenseKeyProps) => {
+  const { classes } = useStyles();
   return (
-    <Paper withBorder px={"xs"} py={"5px"} display={"flex"}>
+    <Paper withBorder className={classes.paper}>
       <Group
         spacing={"xs"}
         style={{ display: "flex", width: "100%", flexWrap: "nowrap" }}
       >
-        <Text size={"sm"} style={{ wordBreak: "break-all" }}>
+        <Text size={"sm"} className={classes.text}>
           {props.value}
         </Text>
         <Box ml={"auto"} display={"flex"}>
