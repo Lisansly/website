a8f12465f559ab57a47e45aeeda9230e9fb74070
yali <tangozyusufali@gmail.com>
2023-06-18 09:38:51 +0300

    Updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,13 +7,11 @@ const useStyles = createStyles((theme) => ({
     width: "100%",
     padding: theme.spacing.xs,
     paddingRight: theme.spacing.xl,
-    borderTop: `1px solid ${
-      theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[3]
-    }`,
+
     backgroundColor:
       theme.colorScheme === "dark"
         ? theme.colors.dark[9]
-        : theme.colors.gray[2],
+        : theme.colors.gray[0],
   },
   text: {
     fontSize: 12.5,
