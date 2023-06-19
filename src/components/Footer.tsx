a9c4bfe7220a316d8d6241cb8c4bc56b848906bf
yali <tangozyusufali@gmail.com>
2023-06-19 15:37:17 +0300

    Updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,45 +1,110 @@
-import { Anchor, Center, Text, createStyles } from "@mantine/core";
+import { Link as ReactRouterDomLink } from "react-router-dom";
+import {
+  createStyles,
+  Overlay,
+  Anchor,
+  Center,
+  Group,
+  Text,
+} from "@mantine/core";
 
 const useStyles = createStyles((theme) => ({
   footer: {
-    position: "sticky",
-    display: "flex",
+    position: "relative",
     width: "100%",
-    padding: theme.spacing.xs,
-    paddingRight: theme.spacing.xl,
-
-    backgroundColor:
-      theme.colorScheme === "dark"
-        ? theme.colors.dark[9]
-        : theme.colors.gray[0],
+    paddingBottom: theme.spacing.xl,
+    paddingTop: 250,
+    display: "grid",
   },
-  text: {
-    fontSize: 12.5,
+  divider: {
+    height: 2,
+    borderRadius: 100,
+    backgroundImage: theme.fn.gradient({
+      from: theme.colors.violet[9],
+      to: theme.colors.blue[7],
+    }),
+    marginBottom: theme.spacing.xl,
+  },
+  overlay: {
+    backgroundColor: theme.colorScheme === "dark" ? "#00000080" : "#ffffff80",
+  },
+  background: {
+    borderTopRightRadius: "100%",
+    borderTopLeftRadius: "100%",
+    zIndex: -1,
+    position: "absolute",
+    bottom: "0px",
+    backgroundImage: theme.fn.gradient({
+      from: theme.colors.violet[9],
+      to: theme.colors.blue[7],
+    }),
+    width: "40%",
+    height: "45%",
   },
   link: {
-    color:
-      theme.colorScheme === "dark"
-        ? theme.colors.gray[0]
-        : theme.colors.dark[9],
+    transition: "200ms ease",
+    ":hover": {
+      color:
+        theme.colors[theme.primaryColor][theme.colorScheme === "dark" ? 4 : 6],
+    },
   },
 }));
 
+type LinkProps = {
+  text: string;
+  path: string;
+};
+
+const links: LinkProps[] = [
+  {
+    text: "Home",
+    path: "/",
+  },
+  {
+    text: "Documentation",
+    path: "/documentation",
+  },
+  {
+    text: "Dashboard",
+    path: "/dashboard/cloud-based-licensing",
+  },
+];
+
 const Footer = () => {
   const { classes } = useStyles();
+
+  const Link = (props: LinkProps) => {
+    return (
+      <ReactRouterDomLink to={props.path} className={classes.link}>
+        {props.text}
+      </ReactRouterDomLink>
+    );
+  };
+
   return (
-    <Center className={classes.footer}>
-      <Text className={classes.text}>
-        Developed by{" "}
-        <Anchor
-          className={classes.link}
-          target="_blank"
-          href="https://github.com/Lisansly"
-        >
-          Lisansly
-        </Anchor>{" "}
-        ❤️
-      </Text>
-    </Center>
+    <Group position="center" className={classes.footer}>
+      <Overlay blur={100} zIndex={0} className={classes.overlay} />
+      <Group display={"grid"} style={{ zIndex: 1 }}>
+        <Group>
+          {links.map((link) => (
+            <Link text={link.text} path={link.path} />
+          ))}
+        </Group>
+        <div className={classes.divider} />
+        <Center>
+          <Text>
+            Developed by{" "}
+            <a target="_blank" href="https://github.com/orgs/Lisansly/people">
+              Lisansly Team
+            </a>{" "}
+            ❤️
+          </Text>
+        </Center>
+      </Group>
+      <Center>
+        <div className={classes.background} />
+      </Center>
+    </Group>
   );
 };
 
