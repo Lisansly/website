38a4679a02e1860df86931a945bb5f1870cf353a
yali <tangozyusufali@gmail.com>
2023-06-20 00:02:04 +0300

    Updated routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,17 +1,16 @@
-import {
-  Route,
-  Routes as ReactRouterDomRouters,
-  useLocation,
-} from "react-router-dom";
-import { useIsAuthenticated } from "react-auth-kit";
 import Dashboard from "./components/dashboard/Dashboard";
+import { RequireAuth, useIsAuthenticated } from "react-auth-kit";
 import PageNotFound from "./components/PageNotFound";
 import Profile from "./components/profile/Profile";
 import SignUp from "./components/auth/SignUp";
 import SignIn from "./components/auth/SignIn";
 import Home from "./components/home/Home";
 import { useEffect } from "react";
-
+import {
+  Routes as ReactRouterDomRouters,
+  useLocation,
+  Route,
+} from "react-router-dom";
 const Routes = () => {
   const isAuthenticated = useIsAuthenticated();
   const { pathname } = useLocation();
@@ -30,10 +29,21 @@ const Routes = () => {
         path="/signin"
         element={isAuthenticated() ? <Home /> : <SignIn />}
       />
-      <Route path="/profile/:tabValue?" element={<Profile />} />
+      <Route
+        path="/profile/:tabValue?"
+        element={
+          <RequireAuth loginPath={"/signin"}>
+            <Profile />
+          </RequireAuth>
+        }
+      />
       <Route
         path="/dashboard/:tabValue?/:projectName?"
-        element={<Dashboard />}
+        element={
+          <RequireAuth loginPath={"/signin"}>
+            <Dashboard />
+          </RequireAuth>
+        }
       />
       <Route path="/" element={<Home />} />
       <Route path="/*" element={<PageNotFound />} />
